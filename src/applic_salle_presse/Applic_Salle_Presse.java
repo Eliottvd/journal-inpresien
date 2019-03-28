7486cb15f667fddd967d005328e636d0835c183d
Eliott <Eliott@LAPTOP-PG597B7R>
2019-03-28 09:30:49 +0100

    commit Hello World

--- src/applic_salle_presse/Applic_Salle_Presse.java
+++ src/applic_salle_presse/Applic_Salle_Presse.java
@@ -15,7 +15,7 @@ public class Applic_Salle_Presse {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Eliott van der Straten-Waillet");
     }
     
 }
