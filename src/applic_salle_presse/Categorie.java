747682d7aa742a5d64eff5d4ff5d20255c2e53db
Eliott <Eliott@LAPTOP-QHT69N73>
2019-05-11 12:02:03 +0200

    Changement catégorie en une classe pas instanciable

--- src/applic_salle_presse/Categorie.java
+++ src/applic_salle_presse/Categorie.java
@@ -8,17 +8,30 @@ package applic_salle_presse;
 /**
  *
  * @author Eliott
+ * 
+ * 
+ * C'est une classe qui existe principalement pour avoir les variables membres statiques
+ * On n'a pas besoin de l'instancier 
+ * -> Abstract ? 
+ * NON, constructeur privé 
+ * J'ai fait ca car si abstract, on peut la dériver et instancier la sousclasse.
+ * De plus, ca porte à confusion : on peut croire que la à été désignée pour être dérivée
+ * 
+ * Donc j'ai mis le constructeur en privé ce qui empêche d'instancier la classe de manière propre
  */
-public class Categorie {
-    String nomCategorie;
+
+
+//Noninstanciable Categorie class
+public class Categorie { 
+    private final String nomCategorie; //Car jamais instaciable
     
-    Categorie(String nc)
+    private Categorie(String nc)       //Empêche l'utilisation autre que par les variables static
     {
         nomCategorie = nc;
     }
     
-    static Categorie POLITIQUE = new Categorie("politique");
-    static Categorie SPORT = new Categorie("sport");
-    static Categorie INTERNATIONNAL = new Categorie("internationnal");
-    static Categorie RAGOT = new Categorie("ragot");
+    public static Categorie POLITIQUE = new Categorie("politique");
+    public static Categorie SPORT = new Categorie("sport");
+    public static Categorie INTERNATIONNAL = new Categorie("internationnal");
+    public static Categorie RAGOT = new Categorie("ragot");
 }
